f205f647eab65b437e4d6d1860bbb7816dda190a
Hiroki Chiyoda <contact@arafipro.com>
2024-02-06 00:05:03 +0900

    CSSを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home
-    </main>
+    <form className="w-96 border-2 mx-auto mt-4 rounded">
+      <h1 className="text-2xl text-center mt-2">名前入力フォーム</h1>
+      <div className="m-2">
+        <label>名前</label>
+        <input
+          className="border-2 rounded-md w-full p-1"
+          placeholder="名前を入力してください"
+        />
+      </div>
+      <div className="m-2">
+        <button
+          className="bg-black text-white rounded-lg w-full mt-4 p-1"
+          type="submit"
+        >
+          送信
+        </button>
+      </div>
+      <div className="text-center my-4">あなたの名前はnameです。</div>
+    </form>
   );
 }
